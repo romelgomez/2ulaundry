beef3e32de6172d4a12d0789f2dcf8c289f28a81
Romel Gomez <romel.gomez@rextie.com>
2020-02-13 13:12:44 -0500

    print a fake list of invoices

--- src/App.js
+++ src/App.js
@@ -1,33 +1,77 @@
 import React from "react";
 import "./App.css";
 
-import { List, Typography } from "antd";
-import "antd/es/list/style/css";
-import "antd/es/typography/style/css";
+import {
+  Table,
+  // Divider,
+  Tag
+} from 'antd';
 
-const data = [
-  "Racing car sprays burning fuel into crowd.",
-  "Japanese princess to wed commoner.",
-  "Australian walks 100km after outback crash.",
-  "Man charged over missing wedding girl.",
-  "Los Angeles battles huge wildfires."
+
+import "antd/es/table/style/css";
+import "antd/es/tag/style/css";
+
+import data from './fake_invoices';
+
+// TODO: 
+// "invoice_number": 0,
+// "total": "3,550.39",
+// "currency": "PEN",
+// "invoice_date": "2020-02-13",
+// "due_date": "2020-02-14",
+// "vendor_name": "ZILLIDIUM",
+// "remittance_address": "428 Bragg Court, Chalfant, Colorado, 4185"
+// "status": "Approved"
+
+const columns = [
+  {
+    title: 'Invoice #',
+    dataIndex: 'invoice_number',
+  },
+  // TODO: TO COMMENT, NOT REQUESTED 
+  {
+    title: 'Status',
+    key: 'status',
+    dataIndex: 'status',
+    render: tag => (
+      <span>
+        {<Tag color={tag === 'pending' ? 'volcano' : 'green'} key={tag}>
+          {tag.toUpperCase()}
+        </Tag>}
+      </span>
+    ),
+  },
+  {
+    title: 'Total',
+    dataIndex: 'total',
+  },
+  {
+    title: 'Currency',
+    dataIndex: 'currency',
+  },
+  {
+    title: 'Invoice Date',
+    dataIndex: 'invoice_date',
+  },
+  {
+    title: 'Due Date',
+    dataIndex: 'due_date',
+  },
+  {
+    title: 'Vendor',
+    dataIndex: 'vendor_name',
+  },
+  {
+    title: 'Remittance Address',
+    dataIndex: 'remittance_address',
+  },
 ];
 
 function App() {
   return (
     <div className="App">
-      <h3 style={{ marginBottom: 16 }}>Invoices List</h3>
-      <List
-        header={<div>Header</div>}
-        footer={<div>Footer</div>}
-        bordered
-        dataSource={data}
-        renderItem={item => (
-          <List.Item>
-            <Typography.Text mark>[ITEM]</Typography.Text> {item}
-          </List.Item>
-        )}
-      />
+      <h3 style={{ marginBottom: 16 }}>2ulaundry</h3>
+      <Table rowKey={record => record.invoice_number} dataSource={data.filter(invoice => invoice.status === 'pending').reverse()} columns={columns} />
     </div>
   );
 }
